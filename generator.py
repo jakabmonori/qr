d1a9af7ffb883d9409e50a0d55ce2618aa54764a
Csaba <csabamonori02@gmail.com>
2021-03-02 17:39:01 +0100

    v5.5.0
    Added export logic to the program. Still have to implement it.

--- generator.py
+++ generator.py
@@ -9,15 +9,22 @@
 import string
 import random
 import qrcode
+from exporter import csv_generator
 
 class Generator:
 
     str_chars = string.ascii_letters + string.digits
 
+    exporter_arr = [None]
+
     qr = qrcode.QRCode(
         version=None,
         error_correction=qrcode.constants.ERROR_CORRECT_H
     )
+
+    def __init__(self, no_of_codes):
+        self.exporter_arr = [None] * int(no_of_codes) 
+
 
     def generate_string(self, string_length):
         result_str = ''.join(random.choice(self.str_chars) for i in range(int(string_length)))
@@ -37,7 +44,23 @@ class Generator:
             self.qr.add_data(result_string)
             self.qr.make(fit=True)
 
+            self.generate_array(x, rnd_string, string_length, result_string)
+
             img = self.qr.make_image(fill_color="red", back_color="white")
             img.save(save_location + '/' + rnd_string + '.png')
             self.qr.clear()
+        print(self.exporter_arr)
+        self.call_exporter(self.exporter_arr)
+
+    def generate_array(self, x, rnd_string, string_length, result_string):
+        self.exporter_arr[x] = [rnd_string, string_length, result_string]
+
+    def call_exporter(self, export_arr):
+        csvgen = csv_generator()
+        csvgen.test(export_arr)
+
+
+
+    
+
 
